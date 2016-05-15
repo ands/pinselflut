d4b7eef8061b4ff4b51814b844ef180b49feb4b6
ands <amanti@gmx.de>
2016-05-15 05:41:16 +0200

    moved to glfw and added nukear gui

--- Makefile
+++
@@ -1,2 +0,0 @@
-all:
-	$(CC) -std=c99 -O3 -o pinselflut `pkg-config --cflags sdl2` pinselflut.c -lm `pkg-config --libs sdl2`
--- pinselflut.c
+++ pinselflut.c
@@ -11,11 +11,21 @@
 #include <netdb.h>
 #include <fcntl.h>
 #include <errno.h>
-#include <SDL.h>
 #include <signal.h>
+#include <math.h>
+#include "glad/glad.h"
+#include <GLFW/glfw3.h>
 
-#define WIDTH 1280
-#define HEIGHT 720
+#define NK_INCLUDE_FIXED_TYPES
+#define NK_INCLUDE_STANDARD_IO
+#define NK_INCLUDE_DEFAULT_ALLOCATOR
+#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
+#define NK_INCLUDE_FONT_BAKING
+#define NK_INCLUDE_DEFAULT_FONT
+#define NK_IMPLEMENTATION
+#define NK_GLFW_GL3_IMPLEMENTATION
+#include "nuklear.h"
+#include "nuklear_glfw_gl3.h"
 
 static inline int itoa(int n, char *s) // positive integers only!
 {
@@ -30,12 +40,6 @@ static inline int itoa(int n, char *s) // positive integers only!
 	return i;
 }
 
-static void error(const char *msg)
-{
-	perror(msg);
-	exit(0);
-}
-
 static char *hostname;
 static int port;
 static int sockfd = 0;
@@ -47,7 +51,10 @@ static void flutConnect()
 	struct hostent *server;
 	server = gethostbyname(hostname);
 	if (server == NULL)
-		error("ERROR no such host\n");
+	{
+		perror("ERROR no such host\n");
+		exit(1);
+	}
 	struct sockaddr_in serv_addr;
 	bzero((char *) &serv_addr, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
@@ -55,27 +62,64 @@ static void flutConnect()
 	serv_addr.sin_port = htons(port);
 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0) 
-		error("ERROR opening socket\n");
+	if (sockfd < 0)
+	{
+		perror("ERROR opening socket\n");
+		exit(2);
+	}
 
 	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEPORT, &(int){ 1 }, sizeof(int)) < 0)
-		error("setsockopt(SO_REUSEPORT) failed\n");
+	{
+		perror("setsockopt(SO_REUSEPORT) failed\n");
+		exit(3);
+	}
 	
 	if (connect(sockfd,(struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
-		error("ERROR connecting\n");
+	{
+		perror("ERROR connecting\n");
+		exit(4);
+	}
 
 	fcntl(sockfd, F_SETFL, fcntl(sockfd, F_GETFL, 0) | O_NONBLOCK);
 	signal(SIGPIPE, SIG_IGN);
 }
 
-SDL_Renderer *renderer;
-SDL_Texture *texture;
+static int pixelsWidth = 640, pixelsHeight = 480;
+static uint8_t *pixels;
+static void readSize()
+{
+	// retrieve server screen resolution using the SIZE command
+	fcntl(sockfd, F_SETFL, fcntl(sockfd, F_GETFL, 0) & (~O_NONBLOCK)); // temporarily disable non-blocking mode
+	int n = write(sockfd, "SIZE\n", 5);
+	if (n == 5)
+	{
+		char result[256];
+		n = read(sockfd, result, 256);
+		if (n > 5 && !strncmp(result, "SIZE ", 5))
+		{
+			int w, h;
+			n = sscanf(result, "SIZE %d %d", &w, &h);
+			if (n == 2)
+			{
+				pixelsWidth = w;
+				pixelsHeight = h;
+				printf("Received screen size from server: %dx%d\n", pixelsWidth, pixelsHeight);
+			}
+			else
+				printf("Bad SIZE payload!\n");
+		}
+		else
+			printf("Bad SIZE response!\n");
+	}
+	else
+		printf("Could not send SIZE command!\n");
+	fcntl(sockfd, F_SETFL, fcntl(sockfd, F_GETFL, 0) | O_NONBLOCK); // reenable non-blocking mode
+}
 
 static unsigned char buffer[1024], *p = buffer;
-static uint8_t *pixels;
 static void setPixel(int x, int y, uint8_t r, uint8_t g, uint8_t b, uint8_t a)
 {
-	if (x < 0 || y < 0 || x >= WIDTH || y >= HEIGHT)
+	if (x < 0 || y < 0 || x >= pixelsWidth || y >= pixelsHeight)
 		return;
 
 	// flush buffer if necessary
@@ -95,7 +139,7 @@ static void setPixel(int x, int y, uint8_t r, uint8_t g, uint8_t b, uint8_t a)
 			}
 			else
 			{
-				printf("ERROR %d writing to socket\n", errno);
+				fprintf(stderr, "ERROR %d writing to socket\n", errno);
 				exit(1);
 			}
 		}
@@ -118,47 +162,63 @@ static void setPixel(int x, int y, uint8_t r, uint8_t g, uint8_t b, uint8_t a)
 	*p++ = '\n';
 	
 	// set pixel locally
-	pixels[(y * WIDTH + x) * 4 + 0] = (pixels[(y * WIDTH + x) * 4 + 0] * (255 - a) + r * a) >> 8;
-	pixels[(y * WIDTH + x) * 4 + 1] = (pixels[(y * WIDTH + x) * 4 + 1] * (255 - a) + g * a) >> 8;
-	pixels[(y * WIDTH + x) * 4 + 2] = (pixels[(y * WIDTH + x) * 4 + 2] * (255 - a) + b * a) >> 8;
+	float alpha = a / 255.0f, nalpha = 1.0f - alpha;
+	uint8_t *pixel = pixels + (y * pixelsWidth + x) * 3;
+	pixel[0] = (uint8_t)(pixel[0] * nalpha + r * alpha);
+	pixel[1] = (uint8_t)(pixel[1] * nalpha + g * alpha);
+	pixel[2] = (uint8_t)(pixel[2] * nalpha + b * alpha);
 }
 
-static void fillPoint(int x, int y, int size, uint8_t r, uint8_t g, uint8_t b)
+static void fillPoint(int x, int y, int size, uint8_t r, uint8_t g, uint8_t b, uint8_t a)
 {
 	float radius = size / 2.0f;
 	x -= (int)ceilf(radius);
 	y -= (int)ceilf(radius);
+	float a2 = powf(a / 255.0f, 1.0f / 5.0f);
 	for (int yi = 0; yi < size; yi++)
 	{
 		float dy = abs(yi - radius);
 		for (int xi = 0; xi < size; xi++)
 		{
 			float dx = abs(xi - radius);
-			float a = 1.0f - (sqrtf(dx * dx + dy * dy) / radius);
-			if (a < 0.0f) a = 0.0f;
-			a = powf(a, 7.0f);
-			if (a > 0.03f)
-				setPixel(x + xi, y + yi, r, g, b, (uint8_t)(a * 255.0));
+			float alpha = a2 * (1.0f - (sqrtf(dx * dx + dy * dy) / radius));
+			if (alpha > 0.0f)
+			{
+				alpha = powf(alpha, 7.0f);
+				alpha *= 255.0f;
+				if (alpha >= 1.0f)
+					setPixel(x + xi, y + yi, r, g, b, (uint8_t)alpha);
+			}
 		}
 	}
 }
 
+struct
+{
+	int x, y, w, h;
+	uint8_t r, g, b, a;
+	int currentLine;
+} fillState = {0};
 static void fillRect(int x, int y, int w, int h, uint8_t r, uint8_t g, uint8_t b, uint8_t a)
 {
-	for (int yi = 0; yi < h; yi++)
+	fillState.x = x; fillState.y = y; fillState.w = w; fillState.h = h;
+	fillState.r = r; fillState.g = g; fillState.b = b; fillState.a = a;
+	fillState.currentLine = 0;
+}
+static int fillUpdate()
+{
+	if (fillState.currentLine < fillState.h)
 	{
-		for (int xi = 0; xi < w; xi++)
-			setPixel(x + xi, y + yi, r, g, b, a);
-		if (!(yi & 1))
-		{
-			SDL_UpdateTexture(texture, NULL, pixels, WIDTH * 4);
-			SDL_RenderCopy(renderer, texture, NULL, NULL);
-			SDL_RenderPresent(renderer);
-		}
+		for (int x = 0; x < fillState.w; x++)
+			setPixel(fillState.x + x, fillState.y + fillState.currentLine,
+				fillState.r, fillState.g, fillState.b, fillState.a);
+		fillState.currentLine++;
+		return 1;
 	}
+	return 0;
 }
 
-static void drawLine(int x0, int y0, int x1, int y1, int radius, uint8_t r, uint8_t g, uint8_t b)
+static void drawLine(int x0, int y0, int x1, int y1, int radius, uint8_t r, uint8_t g, uint8_t b, uint8_t a)
 {
 	int dx = abs(x1 - x0), sx = x0 < x1 ? 1 : -1;
 	int dy = abs(y1 - y0), sy = y0 < y1 ? 1 : -1; 
@@ -166,7 +226,7 @@ static void drawLine(int x0, int y0, int x1, int y1, int radius, uint8_t r, uint
 
 	for(;;)
 	{
-		fillPoint(x0, y0, radius, r, g, b); // TODO: optimize number of sent pixels!
+		fillPoint(x0, y0, radius, r, g, b, a); // TODO: optimize number of sent pixels!
 		if (x0 == x1 && y0 == y1)
 			break;
 		e2 = err;
@@ -175,112 +235,187 @@ static void drawLine(int x0, int y0, int x1, int y1, int radius, uint8_t r, uint
 	}
 }
 
-int main(int argc, char *argv[])
+static void error_callback(int e, const char *d)
+{
+	printf("Error %d: %s\n", e, d);
+}
+
+int main(int argc, char **argv)
 {
 	if (argc < 3)
 	{
-		fprintf(stderr,"usage %s hostname port\n", argv[0]);
+		fprintf(stderr, "usage %s hostname port\n", argv[0]);
 		exit(0);
 	}
 	hostname = argv[1];
 	port = atoi(argv[2]);
 	flutConnect();
-	
-	// TODO: retrieve server resolution with SIZE command!
-	
-	SDL_Init(SDL_INIT_VIDEO);
-	SDL_Window* window = SDL_CreateWindow("pinselflut", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
-	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-	SDL_RenderClear(renderer);
-	texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ABGR8888, SDL_TEXTUREACCESS_STREAMING, WIDTH, HEIGHT);
-	if(!texture)
+	readSize();
+
+	glfwSetErrorCallback(error_callback);
+	if (!glfwInit())
 	{
-		printf("could not create texture\n");
-		SDL_Quit();
-		return 1;
+		fprintf(stderr, "GFLW failed to init!\n");
+		exit(1);
 	}
-	pixels = calloc(WIDTH * HEIGHT * 4, 1);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	#endif
+	GLFWwindow *window = glfwCreateWindow(232 + pixelsWidth, pixelsHeight + 96, "Pinselflut", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+	glfwSwapInterval(1);
 	
-	SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN_DESKTOP);
-	SDL_DisplayMode mode;
-	SDL_GetCurrentDisplayMode(0, &mode);
-	
-	int x = 0, y = 0, draw = 0, erase = 0;
-	uint8_t r = 255, g = 255, b = 255;
-	int size = 7;
+	pixels = calloc(pixelsWidth * pixelsHeight * 3, 1);
+	GLuint texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, pixelsWidth, pixelsHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, pixels);
+
+	struct nk_context *ctx = nk_glfw3_init(window, NK_GLFW3_INSTALL_CALLBACKS);
+	struct nk_font_atlas *atlas;
+	nk_glfw3_font_stash_begin(&atlas);
+	nk_glfw3_font_stash_end();
+
+	#define MAX_DRAW_BUFFER_SIZE 32
+	int x[MAX_DRAW_BUFFER_SIZE] = {0}, y[MAX_DRAW_BUFFER_SIZE] = {0};
+	nk_size drawBufferPos = 0, drawBufferSize = 8;
+	int lx = -1, ly = -1;
+	struct nk_color fg = nk_rgba(255, 255, 255, 255);
+	struct nk_color bg = nk_rgba(0, 0, 0, 255);
+	nk_size size = 7;
 	int idleCounter = 0;
-	while(42)
+	while (!glfwWindowShouldClose(window))
 	{
-		SDL_Event event;
-		if(SDL_PollEvent(&event))
+		glfwPollEvents();
+		int w, h;
+		glfwGetFramebufferSize(window, &w, &h);
+		nk_glfw3_new_frame();
+		
+		if (++idleCounter >= 60)
 		{
-			if (event.type == SDL_QUIT)
-				break;
-			if (event.type == SDL_KEYDOWN)
-			{
-				if (event.key.keysym.sym == SDLK_ESCAPE) // quit
-					break;
-				
-				if (event.key.keysym.sym == SDLK_SPACE) // clear
-					fillRect(0, 0, WIDTH, HEIGHT, 0, 0, 0, 255);
-				
-				if (event.key.keysym.sym == SDLK_r) // step red component
-					r += 16;
-				if (event.key.keysym.sym == SDLK_g) // step green component
-					g += 16;
-				if (event.key.keysym.sym == SDLK_b) // step blue component
-					b += 16;
-				
-				if (event.key.keysym.sym == SDLK_PLUS) // increase brush size
-					size++;
-				if (event.key.keysym.sym == SDLK_MINUS) // decrease brush size
-					size--;
-				if (size < 1) size = 1;
-				if (size > 32) size = 32;
-			}
-			if (event.type == SDL_MOUSEBUTTONDOWN)
-			{
-				if (event.button.button == SDL_BUTTON_LEFT) // draw
-					draw = 1;
-				if (event.button.button == SDL_BUTTON_MIDDLE) // erase
-					erase = 1;
-			}
-			if (event.type == SDL_MOUSEBUTTONUP)
-			{
-				if (event.button.button == SDL_BUTTON_LEFT)
-					draw = 0;
-				if (event.button.button == SDL_BUTTON_MIDDLE)
-					erase = 0;
-			}
-			if (event.type == SDL_MOUSEMOTION)
-			{
-				int nx = WIDTH * event.motion.x / mode.w;
-				int ny = HEIGHT * event.motion.y / mode.h;
+			idleCounter = 0;
+			const char nl = '\n';
+			int n = write(sockfd, &nl, 1); // keep alive
+		}
+		
+		glBindTexture(GL_TEXTURE_2D, texture);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, pixelsWidth, pixelsHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, pixels);
+		struct nk_panel canvas;
+		if (nk_begin(ctx, &canvas, "Canvas", nk_rect(200, 0, pixelsWidth + 32, pixelsHeight + 96),
+			NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|//NK_WINDOW_SCALABLE|
+			NK_WINDOW_MINIMIZABLE|NK_WINDOW_TITLE))
+		{
+			nk_layout_row_static(ctx, pixelsHeight, pixelsWidth, 1);
+			struct nk_vec2 p = nk_widget_position(ctx);
+			nk_image(ctx, nk_image_id(texture));
 
-				if (erase)
-					drawLine(x, y, nx, ny, size * 2, 0, 0, 0);
-				else if (draw)
-					drawLine(x, y, nx, ny, size, r, g, b);
-				x = nx; y = ny;
-			}
-		}
-		else
-		{
-			if (++idleCounter >= 60)
+			if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
 			{
-				idleCounter = 0;
-				const char nl = '\n';
-				int n = write(sockfd, &nl, 1); // keep alive
+				struct nk_color color = fg;
+				if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_MIDDLE) == GLFW_PRESS)
+					color = bg;
+				
+				int nx = ctx->input.mouse.pos.x - p.x;
+				int ny = ctx->input.mouse.pos.y - p.y;
+				x[drawBufferPos] = nx; y[drawBufferPos] = ny;
+				if (++drawBufferPos >= drawBufferSize)
+				{
+					int avgx = 0, avgy = 0;
+					for (int i = 0; i < drawBufferSize; i++)
+					{
+						avgx += x[i];
+						avgy += y[i];
+					}
+					avgx /= drawBufferSize;
+					avgy /= drawBufferSize;
+					if (avgx >= 0 && avgy >= 0 && avgx < pixelsWidth && avgy < pixelsHeight)
+					{
+						if (lx == -1 && ly == -1) { lx = avgx; ly = avgy; }
+						if (avgx != lx || avgy != ly)
+							drawLine(lx, ly, avgx, avgy, size, color.r, color.g, color.b, color.a);
+						lx = avgx; ly = avgy;
+					}
+					
+					for (int i = 0; i < drawBufferSize - 1; i++)
+					{
+						x[i] = x[i + 1];
+						y[i] = y[i + 1];
+					}
+					drawBufferPos--;
+					
+					idleCounter = 0;
+				}
 			}
-			SDL_UpdateTexture(texture, NULL, pixels, WIDTH * 4);
-			SDL_RenderCopy(renderer, texture, NULL, NULL);
-			SDL_RenderPresent(renderer);
+			else
+			{
+				drawBufferPos = 0;
+				lx = -1; ly = -1;
+			}
 		}
+		nk_end(ctx);
+
+		struct nk_panel tools;
+		if (nk_begin(ctx, &tools, "Tools", nk_rect(0, 0, 200, pixelsHeight + 96),
+			NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_SCALABLE|
+			NK_WINDOW_MINIMIZABLE|NK_WINDOW_TITLE))
+		{
+			nk_layout_row_dynamic(ctx, 20, 1);
+			nk_label(ctx, "Brush Size:", NK_TEXT_LEFT);
+			nk_layout_row_dynamic(ctx, 30, 1);
+			nk_progress(ctx, &size, 50, 1);
+			
+			nk_layout_row_dynamic(ctx, 20, 1);
+			nk_label(ctx, "Stabilization:", NK_TEXT_LEFT);
+			nk_layout_row_dynamic(ctx, 30, 1);
+			nk_progress(ctx, &drawBufferSize, MAX_DRAW_BUFFER_SIZE - 1, 1);
+			if (drawBufferSize < 1)
+				drawBufferSize = 1;
+			if (drawBufferSize >= MAX_DRAW_BUFFER_SIZE)
+				drawBufferSize = MAX_DRAW_BUFFER_SIZE - 1;
+
+			nk_layout_row_dynamic(ctx, 20, 1);
+			nk_label(ctx, "Foreground Color:", NK_TEXT_LEFT);
+			nk_layout_row_dynamic(ctx, 120, 1);
+			fg = nk_color_picker(ctx, fg, NK_RGBA);
+
+			nk_layout_row_dynamic(ctx, 20, 1);
+			nk_label(ctx, "Background Color:", NK_TEXT_LEFT);
+			nk_layout_row_dynamic(ctx, 120, 1);
+			bg = nk_color_picker(ctx, bg, NK_RGBA);
+
+			nk_layout_row_dynamic(ctx, 30, 1);
+			if (nk_button_label(ctx, "Foreground Fill", NK_BUTTON_DEFAULT))
+				fillRect(0, 0, pixelsWidth, pixelsHeight, fg.r, fg.g, fg.b, fg.a);
+			nk_layout_row_dynamic(ctx, 30, 1);
+			if (nk_button_label(ctx, "Background Fill", NK_BUTTON_DEFAULT))
+				fillRect(0, 0, pixelsWidth, pixelsHeight, bg.r, bg.g, bg.b, bg.a);
+			if (fillUpdate())
+			{
+				nk_layout_row_dynamic(ctx, 20, 1);
+				nk_label(ctx, "Filling in progress", NK_TEXT_LEFT);
+			}
+		}
+		nk_end(ctx);
+
+		glViewport(0, 0, w, h);
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		nk_glfw3_render(NK_ANTI_ALIASING_ON, 512 * 1024, 128 * 1024);
+		glfwSwapBuffers(window);
 	}
+	nk_glfw3_shutdown();
 	free(pixels);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
-
+	glfwTerminate();
+	
 	close(sockfd);
 	return 0;
 }
+
