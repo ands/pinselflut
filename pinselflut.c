2e853897e5c14d23cc2bf140d72535f8a3fd8f4f
ands <amanti@gmx.de>
2016-05-18 20:31:04 +0200

    Only require a specific OpenGL version on MacOS. This fixes #1

--- pinselflut.c
+++ pinselflut.c
@@ -258,10 +258,10 @@ int main(int argc, char **argv)
 		fprintf(stderr, "GFLW failed to init!\n");
 		exit(1);
 	}
+	#ifdef __APPLE__
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	#ifdef __APPLE__
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	#endif
 	GLFWwindow *window = glfwCreateWindow(232 + pixelsWidth, pixelsHeight + 96, "Pinselflut", NULL, NULL);
